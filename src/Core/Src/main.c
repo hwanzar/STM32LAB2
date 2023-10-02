684601d0f755075dde449efdef203652b76104a6
gia <giadh1103@gmail.com>
2023-10-02 14:37:32 +0700

    minor change

--- src/Core/Src/main.c
+++ src/Core/Src/main.c
@@ -255,7 +255,7 @@ int main(void)
 		  updateClockBuffer();
 	  }
 	  if(timer_flag[3] == 1){
-		  setTimer(3,4);
+		  setTimer(3,5);
 		  updateLEDMatrix(index_led_matrix++);
 		  if(index_led_matrix >= MAX_LED_MATRIX-1) index_led_matrix = 1;
 	  }
